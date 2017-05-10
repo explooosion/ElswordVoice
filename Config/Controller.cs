690b961bacb7f3d539bbc70fa458aa60be918c17
Robby <ta7382@gmail.com>
2017-05-10 23:50:06 +0800

    Initialize Project

---
+++ Config/Controller.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace ElswordVoice.Config
+{
+    class Controller
+    {
+        /**
+          *檔案存取變更方法 － 2016/02/14 IncrEdibLe (Robby)
+          */
+
+        public void RenameToTemp(string path, string[] files)
+        {
+            foreach (string file in files)
+            {
+                if (isExists(path, file))
+                {
+                    try
+                    {
+                        File.Move(Path.Combine(path, file), Path.Combine(path, file + ".temp"));
+                        Application.DoEvents();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+        public void RenameToKom(string path, string[] files)
+        {
+            foreach (string file in files)
+            {
+                if (isExists(path, file))
+                {
+                    try
+                    {
+                        File.Move(Path.Combine(path, file), Path.Combine(path, file.Replace(".temp", "")));
+                        Application.DoEvents();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+        public void CopyCustomFiles(string sourcepath, string targetpath, string[] files)
+        {
+            foreach (string file in files)
+            {
+                if (isExists(sourcepath, file))
+                {
+                    try
+                    {
+                        File.Copy(Path.Combine(sourcepath, file), Path.Combine(targetpath, file), true);    // OverWrite
+                        Application.DoEvents();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+        public void DeleteCustomFiles(string path, string[] files)
+        {
+            foreach (string file in files)
+            {
+                if (isExists(path, file) && isExists(path, file.Replace(".temp", "")))
+                {
+                    try
+                    {
+                        File.Delete(Path.Combine(path, file.Replace(".temp", "")));
+                        Application.DoEvents();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+        }
+
+        public void DeleteRegedit()
+        {
+            RegistryKey regkey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\ElswordINT\TW\PatcherOption\", true);
+
+            if (regkey.OpenSubKey("MARK_INVALID") != null)
+            {
+                regkey.DeleteSubKeyTree("MARK_INVALID");
+            }
+        }
+
+        private bool isExists(string path, string file)
+        {
+            if (File.Exists(Path.Combine(path, file)))
+            {
+                return true;
+            }
+            Console.WriteLine("File:{0},is not exists", file);
+            return false;
+        }
+    }
+}
---
+++ Config/Data.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+
+namespace ElswordVoice.Config
+{
+    class Data
+    {
+        /**
+         * 資料路徑設定 － 2016/02/14 IncrEdibLe (Robby)
+         * 
+         * Elsword －－ Data(.kom / .temp)
+         *      |
+         *      |  －－ ElswordVoice －－ Ex_Voice (.kom)
+         */
+
+        // Folder Path
+        private DirectoryInfo AppPath = new DirectoryInfo(System.Windows.Forms.Application.StartupPath);
+        private string DataPath = string.Empty;
+        private string ExChangePath = string.Empty;
+
+        // Files List
+        private string[] CustomFiles;
+        private string[] RemainFiles;
+
+        public string GetThisPath()
+        {
+            return AppPath.FullName.ToString();
+        }
+
+        public string GetInstallPath()
+        {
+            return AppPath.Parent.FullName.ToString();
+        }
+
+        public string GetDataPath()
+        {
+            DataPath = Path.Combine(AppPath.Parent.FullName.ToString(), @"data\");
+            if (!Directory.Exists(DataPath))
+            {
+                Directory.CreateDirectory(DataPath);
+            }
+            return DataPath;
+        }
+
+        public string GetExChangePath()
+        {
+            ExChangePath = Path.Combine(AppPath.FullName.ToString(), @"Ex_Voice\");
+            if (!Directory.Exists(ExChangePath))
+            {
+                Directory.CreateDirectory(ExChangePath);
+            }
+            return ExChangePath;
+        }
+
+        public string[] GetCustomFiles()
+        {
+            string[] files = Directory.GetFiles(GetExChangePath(), "*.kom");
+            CustomFiles = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                CustomFiles[i] = Path.GetFileName(files[i]);
+            }
+            return CustomFiles;
+        }
+
+        public string[] GetRemainFiles()
+        {
+            string[] files = Directory.GetFiles(GetDataPath(), "*.temp");
+            RemainFiles = new string[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                RemainFiles[i] = Path.GetFileName(files[i]);
+            }
+            return RemainFiles;
+        }
+
+    }
+}
